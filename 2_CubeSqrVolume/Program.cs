98f747039ab79b1d2c174b7c1c7b74f714cae8ff
asengin <worksmail2017@gmail.com>
2021-12-14 16:17:12 +0300

    Задача 2

---
+++ 2_CubeSqrVolume/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CubeSqrVolume
+{
+    class Program
+    {
+        //Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер ребра куба: ");
+            int edgeCube = Convert.ToInt32(Console.ReadLine());
+            if (edgeCube < 0)
+            {
+                Console.WriteLine("Значение должно быть положительным!");
+                Console.ReadKey();
+                return;
+            }
+            int sqrCube, volumeCube;
+            CalcCubeSqrVolume(edgeCube, out sqrCube, out volumeCube);
+            Console.WriteLine($"Объем куба: {volumeCube}. Площадь поверхности куба: {sqrCube}");
+            Console.ReadKey();
+        }
+        static void CalcCubeSqrVolume(int edge, out int sqr, out int volume)
+        {
+            volume = (int)Math.Pow(edge, 3); //Явное преобразование из-за метода Pow возвращающего double. Можно использовать, т.к. сторона куба целое число
+            sqr = edge * edge * 6;
+        }
+    }
+}
---
+++ 2_CubeSqrVolume/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("2_CubeSqrVolume")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2_CubeSqrVolume")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f98c3ca9-01d1-48a8-b513-1e7705247137")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
